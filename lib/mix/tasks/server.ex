39b32c20acbf07bc8d73a04780a6beebe4e3efac
Devon Smith <smithde@oclc.org>
2019-05-07 15:41:28 -0400

    syntax updates to eliminate compiler warnings

--- lib/mix/tasks/server.ex
+++ lib/mix/tasks/server.ex
@@ -14,7 +14,7 @@ defmodule Mix.Tasks.Server do
 
   """
   def run(args) do
-    opts = OptionParser.parse(args, aliases: [h: :host, p: :port]) |> elem(0)
+    opts = OptionParser.parse(args, switches: [host: :string, port: :string], aliases: [h: :host, p: :port]) |> elem(0)
     Mix.Task.run "app.start", args
     Mix.Task.run "compile.sugar", args
 
